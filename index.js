3cf053c15dd7d7cd12772033b5138b4b2157ea81
hellboy <azeemrasydan@gmail.com>
2021-04-13 16:45:20 +0800

    Done adding some important modules

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3000;
 var path = require('path');
 
-
+//Serve static public files API
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
+//Default index file
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/static/public/index.html'));
 });
 
+//Listen on port specified above
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
